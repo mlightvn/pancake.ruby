7e7c86bff63d948cb860fc387e1727a16ec6b12a
Ngoc Nam <thietlalung@yahoo.com.vn>
2017-02-26 23:39:18 +0700

    Posts: demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # get 'posts/detail'
+
+  get 'posts/list'
+  get 'posts/new'
+  post 'posts/create'
+  get 'posts/:id' => 'posts#detail'
+  get 'posts/:id/edit' => 'posts#edit'
+  patch 'posts/:id' => 'posts#update'
+  delete 'posts/:id' => 'posts#destroy'
+
   get 'user/post'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
